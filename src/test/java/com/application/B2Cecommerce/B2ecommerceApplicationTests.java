31840902a1f85a4a17fc16f2f9e56d8d03fe3079
vn0yrph <naresh.naidu@walmart.com>
2020-04-20 15:35:03 +0530

    test cases changes

--- src/test/java/com/application/B2Cecommerce/B2ecommerceApplicationTests.java
+++ src/test/java/com/application/B2Cecommerce/B2ecommerceApplicationTests.java
@@ -3,12 +3,14 @@ package com.application.B2Cecommerce;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.application.B2Cecommerce.controller.BecommerceController;
@@ -37,10 +39,11 @@ class B2ecommerceApplicationTests {
 	}
 	
 	@Test
-	public void createProduct() {
+	public void getAllProduct() {
+		List<Product> list=new ArrayList<>();
 		String result="1";
-		when(service.create(Mockito.any())).thenReturn(result);
-		//assertNotNull(controller.createProduct(getProduct()));
+		when(service.getAllProduct()).thenReturn(list);
+		assertNotNull(controller.getAllProduct());
 	}
 
 }
